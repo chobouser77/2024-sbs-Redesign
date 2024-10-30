be2383f5a6c3d5b8f5e342a0c97a800553417210
chobouser77 <gl8310239@gmail.com>
2024-10-30 19:56:03 +0900

    abc

--- vans_code/js/nav_footer.js
+++ vans_code/js/nav_footer.js
@@ -1,1 +1,31 @@
- 
+// 네비
+$(document).ready(function() {
+
+});
+// 푸터
+$(document).ready(function() {
+  const open_close = $('.oc');
+
+  open_close.click(function(){
+    const submenu = $(this).next('.sub-box');
+    const arrowUp = $(this).find('.arrow-up'); 
+    const arrowDown = $(this).find('.arrow-down');
+
+    if(submenu.hasClass('active')){
+      submenu.removeClass('active');
+      arrowDown.removeClass('active');
+      arrowUp.removeClass('active');
+
+      submenu.css('height','0');
+    }
+    else {
+      submenu.addClass('active');
+      arrowDown.addClass('active');
+      arrowUp.addClass('active');
+      
+      const subHeight = submenu.get(0).scrollHeight;
+      submenu.css('height', subHeight + 'px');
+    }
+  });
+  $('.sub-box').css('height', '0');
+});
