2ac8a002565168f0fadd92771d7adbf72014d84d
ej-201 <fnfffnfkfffkdjswjd@gmail.com>
2024-10-31 18:45:40 +0900

    top-ad-bar, 상단바 1차메뉴

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myblack: "#1d1d1d",
+        myred: "#D41F1F",
+      },
+    }
   },
   plugins: [],
 };
