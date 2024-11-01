33bcf1c0355c7d008e1554f8f7af83c9db781854
chobouser77 <gl8310239@gmail.com>
2024-11-01 20:38:49 +0900

    문제 해결

--- src/js/common.js
+++ src/js/common.js
@@ -1,18 +1,27 @@
 // 헤더
 $(document).ready(function() {
+  
   $(".top-bar__menu01>ul>li").mouseenter(function(){
     $(".pc-top-bar").addClass("on");
     $(".pc-top-bar .logo-color").addClass("on");
     $(".top-bar__user-menu a").addClass("on");
     $(".top-bar__menu02-bg").addClass("on");
+    $(".op").addClass("on");
   });
   $(".top-bar__menu01>ul>li").mouseleave(function(){
     $(".pc-top-bar").removeClass("on");
     $(".pc-top-bar .logo-color").removeClass("on");
     $(".top-bar__user-menu a").removeClass("on");
+    $(".top-bar__menu02-bg").removeClass("on");
+    $(".op").removeClass("on");
   });
-  $(".top-bar__menu02-bg").mouseenter(function(){
-    $(this).removeClass("on");
+  $(".top-bar__menu02-bg").mouseenter(function() {
+    $(this).addClass("on"); // 서브 메뉴가 보일 때 on 클래스 유지
+  });
+
+  // 서브 메뉴에서 마우스가 나갔을 때
+  $(".top-bar__menu02-bg").mouseleave(function() {
+    $(this).removeClass("on"); // 서브 메뉴가 사라질 때 on 클래스 제거
   });
 
 });
