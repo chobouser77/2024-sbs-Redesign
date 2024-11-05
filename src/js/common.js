bf5d32027e4ae12eac842de7bdb2995744019417
chobouser77 <gl8310239@gmail.com>
2024-11-05 18:11:24 +0900

    js오류 수정

--- src/js/common.js
+++ src/js/common.js
@@ -42,123 +42,118 @@ $(document).ready(function () {
     TabletSideMenu__inactiveAll();
   });
 
-// tablet 사이드바 1차메뉴 효과
-$(".tablet-side-bar__menu > ul  li").click(function(){
-  const $this = $(this);
+  // tablet 사이드바 1차메뉴 효과
+  $(".tablet-side-bar__menu > ul  li").click(function () {
+    const $this = $(this);
 
-  $this.addClass("on");
-  $this.siblings(".on").removeClass("on");
-});
-
+    $this.addClass("on");
+    $this.siblings(".on").removeClass("on");
+  });
 
   // tablet 사이드바
-  $(".tablet-side-bar__menu01>ul>li").click(function(){
+  $(".tablet-side-bar__menu01>ul>li").click(function () {
     $(".sidemenu-bg-full").addClass("on");
 
-  //   // $(".tablet-side-bar__menu02").addClass("on");
-  // });
+    //   // $(".tablet-side-bar__menu02").addClass("on");
+    // });
 
-  $(".tablet-side-bar__menu01>ul").click(function(){
-    setTimeout(function() {
-      $(".tablet-side-bar__menu02").addClass("fast-animate");
-    }, 1000);
-  });
-  
-  $(".tablet-side-bar__menu > ul > li > ul > li").click(function () {
-    const $this = $(this);
-  
-    if ($this.hasClass("on")) {
-      $this.removeClass("on");
-    } else {
-      $this.siblings(".on").removeClass("on");
-      $this.addClass("on");
+    $(".tablet-side-bar__menu01>ul").click(function () {
+      setTimeout(function () {
+        $(".tablet-side-bar__menu02").addClass("fast-animate");
+      }, 1000);
+    });
+
+    $(".tablet-side-bar__menu > ul > li > ul > li").click(function () {
+      const $this = $(this);
+
+      if ($this.hasClass("on")) {
+        $this.removeClass("on");
+      } else {
+        $this.siblings(".on").removeClass("on");
+        $this.addClass("on");
+      }
+    });
+
+    $(".tablet-side-bar__menu > ul > li > ul > li > ul").click(function () {
+      return false;
+    });
+
+    function TabletSideMenu__inactiveAll() {
+      $(".tablet-side-bar__menu .on").removeClass("on");
     }
   });
-  
-  $(".tablet-side-bar__menu > ul > li > ul > li > ul").click(function () {
-    return false;
+
+  // 푸터
+  $(document).ready(function () {
+    const open_close = $(".oc");
+
+    open_close.click(function () {
+      const submenu = $(this).next(".sub-box");
+      const arrowUp = $(this).find(".arrow-up");
+      const arrowDown = $(this).find(".arrow-down");
+
+      if (submenu.hasClass("active")) {
+        open_close.removeClass("active");
+        submenu.removeClass("active");
+        arrowDown.removeClass("active");
+        arrowUp.removeClass("active");
+
+        submenu.css("height", "0");
+      } else {
+        open_close.addClass("active");
+        submenu.addClass("active");
+        arrowDown.addClass("active");
+        arrowUp.addClass("active");
+
+        const subHeight = submenu.get(0).scrollHeight;
+        submenu.css("height", subHeight + "px");
+      }
+    });
+    $(".sub-box").css("height", "0");
   });
 
-  function TabletSideMenu__inactiveAll() {
-    $(".tablet-side-bar__menu .on").removeClass("on");
-  }
+  // 메인
+  $(document).ready(function () {
+    const cursor = document.querySelector(".custom-cursor");
+    const mask = document.querySelector(".mask"); // .hover-area를 .mask로 변경
 
-});
+    document.addEventListener("mousemove", (e) => {
+      // 커서의 중앙을 맞추기 위해 마우스 위치에서 커서 크기의 절반을 빼줍니다.
+      cursor.style.left = `${e.pageX - cursor.offsetWidth / 2}px`; // 커서의 중앙을 맞추기 위해
+      cursor.style.top = `${e.pageY - cursor.offsetHeight / 2}px`; // 커서의 중앙을 맞추기 위해
+      cursor.style.display = "block"; // 커서를 보이게 함
+    });
 
-// 푸터
-$(document).ready(function () {
-  const open_close = $(".oc");
+    mask.addEventListener("mouseenter", () => {
+      // .hover-area를 .mask로 변경
+      cursor.style.transform = "scale(10.0)"; // 커서를 1.5배로 확대
+    });
 
-  open_close.click(function () {
-    const submenu = $(this).next(".sub-box");
-    const arrowUp = $(this).find(".arrow-up");
-    const arrowDown = $(this).find(".arrow-down");
+    mask.addEventListener("mouseleave", () => {
+      // .hover-area를 .mask로 변경
+      cursor.style.transform = "scale(1)"; // 커서를 원래 크기로 복원
+    });
 
-    if (submenu.hasClass("active")) {
-      open_close.removeClass("active");
-      submenu.removeClass("active");
-      arrowDown.removeClass("active");
-      arrowUp.removeClass("active");
+    function adjustBoxHeight() {
+      // box의 너비를 가져옴
+      var boxWidth = $(".img-area").width();
 
-      submenu.css("height", "0");
-    } else {
-      open_close.addClass("active");
-      submenu.addClass("active");
-      arrowDown.addClass("active");
-      arrowUp.addClass("active");
-
-      const subHeight = submenu.get(0).scrollHeight;
-      submenu.css("height", subHeight + "px");
+      if ($(window).width() >= 778) {
+        // 스크롤바 제외 768px 이상일 때
+        var boxHeight = (boxWidth - 12) / 2; // 높이를 너비의 절반에서 12px 뺀 값으로 설정
+      } else {
+        // 스크롤바 제외 768px 미만일 때
+        var boxHeight = boxWidth * 2 + 12; // 높이를 너비의 2배에 12px 더한 값으로 설정
+      }
+      // box의 높이를 설정
+      $(".img-area").height(boxHeight);
     }
-  });
-  $(".sub-box").css("height", "0");
-});
-
-// 메인
-$(document).ready(function () {
-  const cursor = document.querySelector(".custom-cursor");
-  const mask = document.querySelector(".mask"); // .hover-area를 .mask로 변경
-
-  document.addEventListener("mousemove", (e) => {
-    // 커서의 중앙을 맞추기 위해 마우스 위치에서 커서 크기의 절반을 빼줍니다.
-    cursor.style.left = `${e.pageX - cursor.offsetWidth / 2}px`; // 커서의 중앙을 맞추기 위해
-    cursor.style.top = `${e.pageY - cursor.offsetHeight / 2}px`; // 커서의 중앙을 맞추기 위해
-    cursor.style.display = "block"; // 커서를 보이게 함
-  });
-
-  mask.addEventListener("mouseenter", () => {
-    // .hover-area를 .mask로 변경
-    cursor.style.transform = "scale(10.0)"; // 커서를 1.5배로 확대
-  });
-
-  mask.addEventListener("mouseleave", () => {
-    // .hover-area를 .mask로 변경
-    cursor.style.transform = "scale(1)"; // 커서를 원래 크기로 복원
-  });
-
-  function adjustBoxHeight() {
-    // box의 너비를 가져옴
-    var boxWidth = $('.img-area').width();
-    
-    if ($(window).width() >= 778) {
-      // 스크롤바 제외 768px 이상일 때
-      var boxHeight = (boxWidth - 12) / 2; // 높이를 너비의 절반에서 12px 뺀 값으로 설정
-  } else {
-      // 스크롤바 제외 768px 미만일 때
-      var boxHeight = (boxWidth * 2) + 12; // 높이를 너비의 2배에 12px 더한 값으로 설정
-  }
-    // box의 높이를 설정
-    $('.img-area').height(boxHeight);
-}
-
-$(document).ready(function () {
     adjustBoxHeight(); // 페이지 로드 시 높이 조정
 
     $(window).resize(function () {
-        adjustBoxHeight(); // 창 크기 조정 시 높이 재조정
+      adjustBoxHeight(); // 창 크기 조정 시 높이 재조정
     });
-});
-
+  });
 });
 
 // 스와이퍼
@@ -200,4 +195,3 @@ $(document).ready(function () {
     }, 300); // 300ms 지연으로 resize 이벤트가 여러 번 호출되는 문제 방지
   });
 });
-
