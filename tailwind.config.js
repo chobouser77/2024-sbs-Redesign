4836836f29cfebacc188be40168d24515891b8af
ej-201 <fnfffnfkfffkdjswjd@gmail.com>
2024-11-08 16:22:27 +0900

    footer-test 끝

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ module.exports = {
         myred: "#D41F1F",
       },
       screens: {
+        "md-2": "900px",
+        "md-3": "1000px",
+        "md-4": "1100px",
         "lg": "1200px",
+        "xl": "1300px",
+        "2xl": "1400px",
+        "3xl": "1500px",
+        "4xl": "1800px",
       },
     }
   },
