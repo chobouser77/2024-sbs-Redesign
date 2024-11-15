58532ab54c1a0a8efb85701570aefec4c139b87f
ej-201 <fnfffnfkfffkdjswjd@gmail.com>
2024-11-15 18:49:29 +0900

    work

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,14 @@ module.exports = {
       colors: {
         myblack: "#1d1d1d",
         myred: "#D41F1F",
+        mybeige: "#FAF6F1",
       },
       screens: {
         "xs": "450px",
         "md-2": "1000px",
         "lg": "1200px",
         "lg-2": "1480px",
+        "2xl": "1600px",
         "3xl": "1700px",
       },
     }
